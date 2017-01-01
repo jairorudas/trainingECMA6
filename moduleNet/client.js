15bbe12ab192c5f17ad15af8cd7e8aa9415e203e
jairo rudas <jairo.enrique@celulardireto.com.br>
2017-01-01 20:44:12 -0200

    explicação do porque dois objeto são iguais as vezes sim as vezes não

---
+++ moduleNet/client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const client = net.connect({host: '127.0.0.1', port: 3000});
+
+process.stdin.on('readable', () => {
+  //O lido aqui é um buffer.
+  let message = process.stdin.read();
+
+  if (!message) return;
+
+  // Conversão do buffer para string.
+  message = message.toString().replace(/\n/, '');
+
+  //envio de messages para o servidor aqui a baixo.
+  client.write(message);
+});
+
+//client.on('connect', () => client.write('Olá, eu sou um cluennte') )
+
+
+// O codigo a baixo faz com que apareça tudos os mensagens enviados desde o servidor.
+client.on('data', (message) => {
+  console.log(message.toString());
+})
---
+++ moduleNet/server.js
@@ -0,0 +1,62 @@
+const net = require('net');
+
+const conections = [];
+
+//Função que envia mensagens a todos os sockets conectados na porta 3000
+let broadcast = (message, origin) => {
+  conections.forEach((conection) => {
+
+     //console.log(conection );
+
+     /*
+      Aqui acontece uma coisa curiosa:
+      Se eu tento comparar dois objeto identicos como por exemplo:
+      var k = {nome: 'luis'};
+      var h = {nome: 'luis'};
+      console.log(k === h) // false
+      o resultado é false porque eles estão apontando para posições em
+      memoria diferentes e javascript trata eles como coisas independente,
+      ainda que seu conteudo for o mesmo.
+
+      NodeJs identifica  o objeto CONNECTION de onde veio a mensagem e EXTRAI
+      esse objeto da memoria (pois já é uma conecção valida), consecuentemente
+      essa conecção deve estar no objeto array que chamamos CONECTIONS.
+
+      Para ser mais ilustrativos vamos ver esse exemplo:
+      Quando um usuario se conectou foi criado OBJ1 e criou um registro em
+      memoria chamado Memoria0001 aí está guardado o objeto CONNECTION com toda
+      as informações da conecção.
+
+      Depois esse usuario envia uma mensagem e NodeJs cria um registro OBJ2
+      apontando para a mesma posição de memoria. Então se a informação da
+      posição de memoria => memoria0001 mudar ONJ1 e OBJ2 também vão mudar
+      porque eles são uma copia dinamica do que esta nessa posição. Por isso
+      é posivel comparar dois objeto e eles serem TRUE SE E SÓ SE ELES ESTÃO
+      APONTANDO PARA A MESMO LUGAR EM MEMORIA.
+
+      OBJ1 === Memoria0001 === OBJ2
+
+      Aqui temos esse caso anterior, origin e conection estão apontando para o
+      mesmo lugar em memoria.
+     */
+    if (conection === origin) return;
+    conection.write(message);
+  })
+};
+
+//criando uma conecção, e enviando feedback
+net.createServer((connection) => {
+  //adicionando conecções ao array CONECTIONS.
+  conections.push(connection);
+
+  connection.write('Servidor Online ;)');
+
+  connection.on('data', (message) => {
+
+    /* O servidor além de receber a mensagem, recebe a conecção de onde veio
+       a mensagem.
+    */
+
+    broadcast(message, connection);
+  });
+}).listen(3000);
