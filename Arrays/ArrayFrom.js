0920c31e84c34f59f7f348095022bb2e14dc8c30
jairorudas <jairorudasp@gmail.com>
2018-05-28 11:35:05 -0300

    Reubicação dos metodos Array.

---
+++ Arrays/ArrayFrom.js
@@ -0,0 +1,9 @@
+/**
+ * Array from captura seja um nodo html ou uma string e tranforma em Array. Não funciona para Objectos.
+ */
+
+const company = 'Reserva'
+
+console.log('------------------------------------');
+console.log(Array.from(company));
+console.log('------------------------------------');
---
+++ Arrays/ArrayOf.js
@@ -0,0 +1,2 @@
+const array = Array.of(1,4, 'Willian', {name: 'Jonas'});
+console.log(array)
