04fd58ce547239bf20cef142270847e874045761
jairorudas <jairorudasp@gmail.com>
2018-05-28 11:42:48 -0300

    Array fill

---
+++ Arrays/ArrayFill.js
@@ -0,0 +1,10 @@
+const arr = [1,2,4]
+
+console.log('------------------------------------');
+/**
+ * @argument 1 o peirmeiro parametro que recebe fill é com que  deve ser preenchido op array
+ * @argument 2 segundo parametro posição onde iniciará o preenchimento
+ * @augments 3 onde deve parar o preenchimento do array
+ */
+console.log(arr.fill(3,1,2));
+console.log('------------------------------------');
