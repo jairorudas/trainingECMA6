c2b7bc5b013e82b36d92a7afab065c85bb5d8d78
jairorudas <jairorudasp@gmail.com>
2018-05-28 09:50:57 -0300

    Novo scopos criados a partir de chaves.

--- scopo.js
+++ scopo.js
@@ -8,3 +8,19 @@ callbacks[0]() === 0
 callbacks[1]() === 2
 callbacks[2]() === 4
 console.log(callbacks);
+
+/**
+ * Scopos de variaveis por blocos.
+ * vemos que podemos criar blocos de codigo com chave e encapsular variaveis.
+ */
+
+{
+    let age = 30
+    console.log(age)
+  }
+ 
+ 
+  {
+    let age = 60
+    console.log(age)
+  }
